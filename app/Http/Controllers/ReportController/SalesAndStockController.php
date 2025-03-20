a3b3763d656bbebb305c39288f0b6c004d197711
kingaseldon <kingaseldon36@gmail.com>
2025-03-20 11:57:13 +0600

    d

--- app/Http/Controllers/ReportController/SalesAndStockController.php
+++ app/Http/Controllers/ReportController/SalesAndStockController.php
@@ -17,7 +17,7 @@ class SalesAndStockController extends Controller
     public function __construct()
     {
 
-        $this->middleware('permission:reports.salesandstocks')->only('stock');
+        $this->middleware('permission:reports.salesandstocks')->only('index');
     }
     public function index(Request $request)
     {
